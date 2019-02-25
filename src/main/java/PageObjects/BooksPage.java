06fbced865973e4991b107851657647c28452d6d
mazgajmaciek <mazgajmaciek@gmail.com>
2019-02-25 21:51:56 +0100

    refactor

--- src/main/java/PageObjects/BooksPage.java
+++ src/main/java/PageObjects/BooksPage.java
@@ -4,14 +4,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 public class BooksPage extends Page {
 
@@ -123,74 +122,42 @@ public class BooksPage extends Page {
     }
 
     public boolean checkIfBookCreatedByName(String newBookTitle) {
-        //TODO - List<WebElement> list won't work here, because when it gets created within the method, the references to webpage elements might have already changed
-//        List<WebElement> list = booksListTitle();
-//        String lastAddedBookTitle = list.get(list.size() - 1).getText();
-        //TODO lastBookTitle() always return last bookList element via xpath
-//        WebDriverWait wait = new WebDriverWait(driver, 5);
-//        wait.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        //TODO - looks like I need to wait for book to be added to the list before checking the new title - waiting for AJAX to finish?
-        //driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        waitForJSandJQueryToLoad();
         return lastBookTitle().getText().equals(newBookTitle);
-//            WebElement el = lastBookTitle();
-//            String elS = el.getText();
-//            boolean bools = elS.equals(newBookTitle);
-//            return bools;
     }
-
-//    public boolean clickDescButton() {
-//
-//        //TODO - calling WebDRiverWait within PageObject method?
-//        WebDriverWait wait = new WebDriverWait(driver, 5);
-//        return wait.until(ExpectedConditions.attributeToBe(lastBookDescription(), "style", "display: block;"));
-//    }
 
     public boolean checkIfDescriptionCreatedByName(String newBookDescription) {
-        lastBookDescButton().click();
+        Actions action = new Actions(driver);
+        action.moveToElement(lastBookDescButton()).click().perform();
         waitForJSandJQueryToLoad();
-//        clickDescButton();
         return lastBookDescription().getText().equals(newBookDescription);
-//        WebElement descriptionBtn = bookDescriptionButton;
-//        WebElement lastListButton = bookListButtons.get(bookListButtons.size() - 1);
-//        lastListButton.click();
-//
-//        return true;
-//
-//        WebDriverWait wait = new WebDriverWait(driver, 5);
-//        descriptionBtn = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("[style=\"display: block;\"]")));
-//
-//        String lastAddedBookDescription = listDescription.get(listDescription.size() - 1).getText();
-//        if (!listDescription.isEmpty()) {
-//                return lastAddedBookDescription.equals(newBookDescription);
-//        }
-//        return false;
     }
 
-//    public void editBook(String newTitle, String newDescription) {
-//        //select last added book
-//        Select editBookDropdown = new Select(bookEditSelect);
-//        int lastOptionIndex = editBookDropdown.getOptions().size() - 1;
-//        editBookDropdown.selectByIndex(lastOptionIndex);
-//
-//        //edit book title
-//        WebElement editBookTitle = bookEditTitle;
-//        editBookTitle.clear();
-//        editBookTitle.sendKeys(newTitle);
-//
-//        //select last available author
-//        Select authorEditDropdown = new Select(bookEditAuthor);
-//        int lastAuthorIndex = authorEditDropdown.getOptions().size() - 1;
-//        authorEditDropdown.selectByIndex(lastAuthorIndex);
-//
-//        //edit book description
-//        WebElement editBookDescription = bookEditDescription;
-//        editBookDescription.clear();
-//        editBookDescription.sendKeys(newDescription);
-//
-//        //submit edited book
-//        WebElement editBook = editBookBtn;
-//        editBook.submit();
-//    }
+    public void editBook(String newTitle, String newDescription) {
+        //select last added book
+        Select editBookDropdown = new Select(bookEditSelect);
+        int lastOptionIndex = editBookDropdown.getOptions().size() - 1;
+        editBookDropdown.selectByIndex(lastOptionIndex);
+
+        //edit book title
+        WebElement editBookTitle = bookEditTitle;
+        editBookTitle.clear();
+        editBookTitle.sendKeys(newTitle);
+
+        //select last available author
+        Select authorEditDropdown = new Select(bookEditAuthor);
+        int lastAuthorIndex = authorEditDropdown.getOptions().size() - 1;
+        authorEditDropdown.selectByIndex(lastAuthorIndex);
+
+        //edit book description
+        WebElement editBookDescription = bookEditDescription;
+        editBookDescription.clear();
+        editBookDescription.sendKeys(newDescription);
+
+        //submit edited book
+        WebElement editBook = editBookBtn;
+        editBook.submit();
+    }
 
     public void removeBook() {
 
--- src/test/java/FunctionalTests/BooksPageTest.java
+++ src/test/java/FunctionalTests/BooksPageTest.java
@@ -8,8 +8,8 @@ import org.junit.Test;
 public class BooksPageTest extends BaseTest {
 
     private static BooksPage booksPage;
-    private String newBookTitle = "new_title13213213123";
-    private String newBookDescription = "new_title_description321321312";
+    private String newBookTitle = "new_title1321123";
+    private String newBookDescription = "new_title_description32312";
     private String editBookTitle = "edit_title";
     private String editBookDescription = "edit_description";
 
@@ -27,19 +27,18 @@ public class BooksPageTest extends BaseTest {
         booksPage.addBookDesc(newBookDescription);
         booksPage.addBook();
 
-        super.waitForJSandJQueryToLoad();
         Assert.assertTrue(booksPage.checkIfBookCreatedByName(newBookTitle));
         Assert.assertTrue(booksPage.checkIfDescriptionCreatedByName(newBookDescription));
     }
 
-//    @Test
-//    public void editBook() {
-//        booksPage.editBook(editBookTitle, editBookDescription);
-//        Assert.assertTrue(booksPage.checkIfBookCreatedByName(editBookTitle));
-//
-//        //TODO - element not clickable
-//        Assert.assertTrue(booksPage.checkIfDescriptionCreatedByName(editBookDescription));
-//    }
+    @Test
+    public void editBook() {
+        booksPage.editBook(editBookTitle, editBookDescription);
+        Assert.assertTrue(booksPage.checkIfBookCreatedByName(editBookTitle));
+
+        //TODO - element not clickable
+        Assert.assertTrue(booksPage.checkIfDescriptionCreatedByName(editBookDescription));
+    }
 
     @Test
     public void removeBook() {
